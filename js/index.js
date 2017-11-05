6bb93114c809a28bf4f20cb15d6508b780c9a8f8
Luca Marcelli <ionix@protonmail.com>
2017-11-05 23:11:58 +0100

    add editor

--- js/index.js
+++ js/index.js
@@ -1,20 +1,38 @@
-var keystone = new ks.Keystone(ks.ARCH_X86, ks.MODE_64);
-keystone.option(ks.OPT_SYNTAX, ks.OPT_SYNTAX_INTEL);
+var asmInput = ace.edit("assembly");
+asmInput.$blockScrolling = Infinity;
+asmInput.getSession().setMode("ace/mode/assembly_x86");
+asmInput.setTheme("ace/theme/dawn");
+asmInput.setOption("showPrintMargin", false);
+asmInput.setOption("showInvisibles", false);
 
-function toHex(d) {
-    return  ("0"+(Number(d).toString(16))).slice(-2).toUpperCase()
-}
+var bytesOutput = ace.edit("bytes");
+bytesOutput.$blockScrolling = Infinity;
+bytesOutput.setTheme("ace/theme/dawn");
+bytesOutput.setOption("showPrintMargin", false);
+bytesOutput.setOption("displayIndentGuides", false);
+bytesOutput.setOption("readOnly", true);
 
-document.querySelector('#assembly').addEventListener('input', () => {
-	let asm = document.querySelector('#assembly').value;
-	let bytes = keystone.asm(asm);
-	let bytesString = "";
-	for(let i = 0; i < bytes.length; i++) {
-		bytesString += toHex(parseInt(bytes[i])) + " ";
+initKeystone();
+
+function initKeystone() {
+	var keystone = new ks.Keystone(ks.ARCH_X86, ks.MODE_64);
+	keystone.option(ks.OPT_SYNTAX, ks.OPT_SYNTAX_INTEL);
+
+	function toHex(d) {
+	    return  ("0"+(Number(d).toString(16))).slice(-2).toUpperCase()
 	}
-	document.querySelector('#bytes').value = bytesString;
-});
 
-window.onunload = function() {
-    keystone.close();
+	document.querySelector('#assembly').addEventListener('input', () => {
+		let asm = asmInput.getValue();
+		let bytes = keystone.asm(asm);
+		let bytesString = "";
+		for(let i = 0; i < bytes.length; i++) {
+			bytesString += toHex(parseInt(bytes[i])) + " ";
+		}
+		bytesOutput.setValue(bytesString);
+	});
+
+	window.onunload = function() {
+	    keystone.close();
+	}
 }
