57b9de87b3fa27068f7ca26fec192e43a637ef1d
Luca Marcelli <ionix@protonmail.com>
2017-11-06 01:21:58 +0100

    concept is implemented

--- js/index.js
+++ js/index.js
@@ -1,35 +1,73 @@
-var asmInput = ace.edit("assembly");
-asmInput.$blockScrolling = Infinity;
-asmInput.getSession().setMode("ace/mode/assembly_x86");
-asmInput.setTheme("ace/theme/dawn");
-asmInput.setOption("showPrintMargin", false);
-asmInput.setOption("showInvisibles", false);
+var arch = ks.ARCH_X86;
+var mode = ks.MODE_64;
+var syntax = ks.OPT_SYNTAX_INTEL;
+var keystone;
 
-var bytesOutput = ace.edit("bytes");
-bytesOutput.$blockScrolling = Infinity;
-bytesOutput.setTheme("ace/theme/dawn");
-bytesOutput.setOption("showPrintMargin", false);
-bytesOutput.setOption("displayIndentGuides", false);
-bytesOutput.setOption("readOnly", true);
+var asmInput;
+var bytesOutput;
 
+initEditor();
 initKeystone();
 
+function initEditor() {
+	asmInput = ace.edit('assembly');
+	asmInput.$blockScrolling = Infinity;
+	asmInput.getSession().setMode('ace/mode/assembly_x86');
+	asmInput.setTheme('ace/theme/dawn');
+	asmInput.setOption('showPrintMargin', false);
+	asmInput.setOption('showInvisibles', false);
+	asmInput.setOption('fontSize', 22);
+
+	bytesOutput = ace.edit('bytes');
+	bytesOutput.$blockScrolling = Infinity;
+	bytesOutput.setTheme('ace/theme/dawn');
+	bytesOutput.setOption('showPrintMargin', false);
+	bytesOutput.setOption('displayIndentGuides', false);
+	bytesOutput.setOption('readOnly', true);
+	bytesOutput.setOption('fontSize', 22);
+}
+
+function initKeystoneEngine() {
+	if(keystone !== undefined) {
+		keystone.close();
+	}
+	keystone = new ks.Keystone(arch, mode);
+	keystone.option(ks.OPT_SYNTAX, syntax);
+}
+
 function initKeystone() {
-	var keystone = new ks.Keystone(ks.ARCH_X86, ks.MODE_64);
-	keystone.option(ks.OPT_SYNTAX, ks.OPT_SYNTAX_INTEL);
+	initKeystoneEngine();
 
 	function toHex(d) {
-	    return  ("0"+(Number(d).toString(16))).slice(-2).toUpperCase()
+	    return  ('0'+(Number(d).toString(16))).slice(-2).toUpperCase()
 	}
 
-	document.querySelector('#assembly').addEventListener('input', () => {
-		let asm = asmInput.getValue();
-		let bytes = keystone.asm(asm);
-		let bytesString = "";
-		for(let i = 0; i < bytes.length; i++) {
-			bytesString += toHex(parseInt(bytes[i])) + " ";
+	function toArray(hex) {
+		hex = hex.replace(/\n/g, ' ');
+		return '0x' + hex.replace(/ /g, ', 0x');
+	}
+
+	asmInput.on('input', () => {
+		let asm = asmInput.getValue().split('\n');
+		let bytesString = '';
+		for(let j = 0; j < asm.length; j++) {
+			let bytes = keystone.asm(asm[j]);
+			for(let i = 0; i < bytes.length; i++) {
+				bytesString += toHex(parseInt(bytes[i]));
+				if(i !== bytes.length - 1) {
+					bytesString += ' ';
+				}
+			}
+			if(j !== asm.length - 1) {
+				bytesString += '\n';
+			}
 		}
-		bytesOutput.setValue(bytesString);
+		bytesOutput.setValue(bytesString, 1);
+		let array = toArray(bytesString);
+		$('#array').text(
+`[ ` + array + ` ]
+{ ` + array + ` }`
+		);
 	});
 
 	window.onunload = function() {
